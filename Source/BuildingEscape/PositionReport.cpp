db1680c1d0d3fcdf460f076ed9f30dbe2b6f306d
Alan Szmyt <szmyty@gmail.com>
2018-09-21 00:22:10 -0400

    ToggleCompletionMode Updated Build with GetOwner

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -19,7 +19,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString objectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *objectName);
 	
 }
 
